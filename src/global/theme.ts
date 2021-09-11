9876c2856fc30d5cc42bbe04c414f035585f29e1
Arthur Bicalho <arthurbic95@gmail.com>
2021-09-10 23:04:13 -0300

    NuClone

---
+++ src/global/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    primary: '#830ad1',
+   
+    letter: 'rgba(0,0,0,0.7)',
+    background: '#ABB1CC',
+    header: '#DDE3F0',
+
+  },
+
+  fonts: {
+    title700: 'Roboto_700Bold',
+    title500: 'Roboto_500Medium',
+    text400: 'Roboto_400Regular',
+  }
+};
